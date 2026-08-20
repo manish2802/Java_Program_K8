535203f3c7dbc147ce0884e772e6834d706ed75b
manish2802 <manishverma.java@gmail.com>
2026-08-20 15:41:28 +0530

    add kubernets

--- src/main/java/com/professionalit/controller/JavaProgramController.java
+++ src/main/java/com/professionalit/controller/JavaProgramController.java
@@ -1,13 +1,50 @@
 package com.professionalit.controller;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.professionalit.dto.Employee;
 
 @RestController
 public class JavaProgramController {
 
-	@GetMapping
-	public void getGroupByEmploySalary() {
+	@GetMapping("/Java8SecondHighestSalaryDemo")
+	public ResponseEntity<Map<String, Optional<Employee>>> getSecondHighestSalary() {
 
+	    List<Employee> employees = List.of(
+	            new Employee(1, "Amit", "IT", 80000),
+	            new Employee(2, "Rahul", "IT", 90000),
+	            new Employee(3, "John", "IT", 70000),
+
+	            new Employee(4, "Priya", "HR", 60000),
+	            new Employee(5, "Neha", "HR", 75000),
+	            new Employee(6, "Ravi", "HR", 65000),
+
+	            new Employee(7, "Raj", "Sales", 50000),
+	            new Employee(8, "Vikas", "Sales", 80000),
+	            new Employee(9, "Anil", "Sales", 70000)
+	    );
+
+	    Map<String, Optional<Employee>> result =
+	            employees.stream()
+	                    .collect(Collectors.groupingBy(
+	                            Employee::getDepartment,
+	                            Collectors.collectingAndThen(
+	                                    Collectors.toList(),
+	                                    list -> list.stream()
+	                                            .sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+	                                            .skip(1)
+	                                            .findFirst()
+	                            )
+	                    ));
+
+	    return ResponseEntity.ok(result);
 	}
 }
