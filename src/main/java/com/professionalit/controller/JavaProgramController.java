13af150c0c54c85c1139b4635dc4a1b26bb41fe3
manish2802 <manishverma.java@gmail.com>
2026-08-20 13:53:20 +0530

    Add Dockerfile for Spring Boot application

---
+++ src/main/java/com/professionalit/controller/JavaProgramController.java
@@ -0,0 +1,13 @@
+package com.professionalit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JavaProgramController {
+
+	@GetMapping
+	public void getGroupByEmploySalary() {
+
+	}
+}
---
+++ src/test/java/com/professionalit/JavaProgramK8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professionalit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProgramK8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
