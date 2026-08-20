42178b7da8692db8883c326a50fc29a787009a75
manish2802 <manishverma.java@gmail.com>
2026-08-20 18:37:08 +0530

    java

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -7,4 +7,13 @@ function callApi() {
         .catch(error => {
             console.error("API Error:", error);
         });
+        
+          fetch("/Java8-second-highest-salary-number")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("2").innerText = data;
+        })
+        .catch(error => {
+            console.error("API Error:", error);
+        });
 }
