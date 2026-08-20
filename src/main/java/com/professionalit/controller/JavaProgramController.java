5dd317c86cc12baaeb78bb6f39432bc4dace0726
manish2802 <manishverma.java@gmail.com>
2026-08-20 18:21:56 +0530

    api

--- src/main/java/com/professionalit/controller/JavaProgramController.java
+++ src/main/java/com/professionalit/controller/JavaProgramController.java
@@ -15,36 +15,36 @@ import com.professionalit.dto.Employee;
 @RestController
 public class JavaProgramController {
 
+	@GetMapping("/Java8-second-highest-salary-number")
+	public ResponseEntity<Integer> secondHighestSalaryNumber() {
+		List<Integer> salaries = List.of(50000, 80000, 90000, 70000);
+
+		Integer secondHighest = salaries.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst()
+				.orElse(null);
+
+		System.out.println(secondHighest);
+		return ResponseEntity.ok(secondHighest);
+	}
+
 	@GetMapping("/Java8SecondHighestSalaryDemo")
 	public ResponseEntity<Map<String, Optional<Employee>>> getSecondHighestSalary() {
 
-	    List<Employee> employees = List.of(
-	            new Employee(1, "Amit", "IT", 80000),
-	            new Employee(2, "Rahul", "IT", 90000),
-	            new Employee(3, "John", "IT", 70000),
+		List<Employee> employees = List.of(new Employee(1, "Amit", "IT", 80000), new Employee(2, "Rahul", "IT", 90000),
+				new Employee(3, "John", "IT", 70000),
 
-	            new Employee(4, "Priya", "HR", 60000),
-	            new Employee(5, "Neha", "HR", 75000),
-	            new Employee(6, "Ravi", "HR", 65000),
+				new Employee(4, "Priya", "HR", 60000), new Employee(5, "Neha", "HR", 75000),
+				new Employee(6, "Ravi", "HR", 65000),
 
-	            new Employee(7, "Raj", "Sales", 50000),
-	            new Employee(8, "Vikas", "Sales", 80000),
-	            new Employee(9, "Anil", "Sales", 70000)
-	    );
+				new Employee(7, "Raj", "Sales", 50000), new Employee(8, "Vikas", "Sales", 80000),
+				new Employee(9, "Anil", "Sales", 70000));
 
-	    Map<String, Optional<Employee>> result =
-	            employees.stream()
-	                    .collect(Collectors.groupingBy(
-	                            Employee::getDepartment,
-	                            Collectors.collectingAndThen(
-	                                    Collectors.toList(),
-	                                    list -> list.stream()
-	                                            .sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
-	                                            .skip(1)
-	                                            .findFirst()
-	                            )
-	                    ));
+		Map<String, Optional<Employee>> result = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment,
+						Collectors.collectingAndThen(Collectors.toList(),
+								list -> list.stream().sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+										.skip(1).findFirst())));
 
-	    return ResponseEntity.ok(result);
+		return ResponseEntity.ok(result);
 	}
+
 }
